7207c05ee5cde1583eab73b22a5d4489b830a7b1
Ericallibalogun <allieric28@gmail.com>
2024-12-05 08:56:57 +0100

    Add files via upload

---
+++ module2_1.py
@@ -0,0 +1,12 @@
+x = 2
+y = 3
+
+print(' x =', x)
+print('Value of' , x, '+' , 'is' , (x + x))
+print('x =')
+print((x + y), '=' ,  (y + x))
+
+rating = int(input('Enter an integer rating between 1 and 10:  '))
+rating = input('Enter an integer rating between 1 and 10')
+
+
---
+++ module2_3.py
@@ -0,0 +1,5 @@
+grade = 0
+course = 0
+if grade >= 90:
+	course = grade
+print('Congratulations! Your grade of 91 earns you an A in this course')
