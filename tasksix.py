00197a50110b32a9e7be9ba9cf71ad6cd42fa5be
Eric Alli-balogun <allieric28@gmail.com>
2024-12-08 21:23:39 +0100

    Add files via upload

---
+++ tasksix.py
@@ -0,0 +1,20 @@
+firstmultiple =1
+
+for number in range(5):
+	print('the multiples of 4 are:' )
+	count = 4
+
+	firstmultiple *= count
+	print(firstmultiple)
+
+	
+secondmultiple =1
+
+for number in range(5):
+
+	count = 8
+
+	secondmultiple *= count
+	print('the multiples of 8 are: ')
+	print(secondmultiple)
+	
